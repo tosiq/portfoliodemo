bdf7cd575ab9c38191c4feb2f5c94cabd5135a77
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-04-25 19:50:48 +0500

    work done

---
+++ WebApplication16/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication16.Models;
+
+namespace WebApplication16.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        portfolioEntities db = new portfolioEntities();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(datta o)
+        {
+            db.dattas.Add(o);
+            db.SaveChanges();
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
